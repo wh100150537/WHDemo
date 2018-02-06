2bbd9318aa38b3fba6e8ceb814b2901df8f0b57f
wang <1241322642@qq.com>
2018-02-06 18:04:18 +0800

    合并测试

--- src/main/java/com/wh/MyApplication.java
+++ src/main/java/com/wh/MyApplication.java
@@ -13,8 +13,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MyApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
 
-
-
+        ////dddddddddddddddddd
+        //ddddddddddd
         SpringApplication.run(MyApplication.class,args);
     }
 }
